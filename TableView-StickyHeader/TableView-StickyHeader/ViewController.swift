437a1b0226ff80f20c27853263740a58c45fe768
Paul Solt <PaulSolt@gmail.com>
2020-05-08 12:58:00 -0400

    Added starter project

---
+++ TableView-StickyHeader/TableView-StickyHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView-StickyHeader
+//
+//  Created by Paul Solt on 5/8/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
